c6b51bf6ea32a6ce1c3fecc02f70c2f268f7c7ab
William Bennett <william.bennett@lexisnexis.com>
2021-09-15 22:38:38 -0400

    added common scrape function, condensed url lists into one

--- TCG_Scraper_Ver2.0.py
+++ TCG_Scraper_Ver2.0.py
@@ -20,6 +20,81 @@ def main():
     spreadsheet = client.open('Square TCG Scraper Ver1.1')
     sheet = spreadsheet.get_worksheet(1)
 
+    rows = sheet.get_all_records()
+    # Get all of the URLs in one object
+    da_url_list = sheet.batch_get(['Y2:Y' + str(len(rows) + 1)])[0]
+    tcg_url_list = sheet.batch_get(['Z2:Z' + str(len(rows) + 1)])[0]
+    tnt_url_list = sheet.batch_get(['AA2:AA' + str(len(rows) + 1)])[0]
+    cs_url_list = sheet.batch_get(['AB2:AB' + str(len(rows) + 1)])[0]
+
+    total_list = da_url_list + tcg_url_list + tnt_url_list + cs_url_list
+    scrape_url_list(total_list, sheet, headers)
+
+
+def scrape_url_list(urls, sheet, headers):
+    row = 2
+    for start_index in range(0, len(urls), 40):
+        batch_list = []
+        for url in urls[start_index:start_index + 10]:  # The first item is a list containing all the information
+            output_range = "A" + str(row) + ":F" + str(row)
+
+            if url:
+                print("Reading url info for ", "line:", row, url[0])
+
+                try:
+                    response = requests.get(url[0], headers)
+                except requests.exceptions.MissingSchema:
+                    return
+
+                soup = BeautifulSoup(response.content, features="lxml")
+
+                title = soup.find('h1').get_text().strip()
+                try:
+                    # Grab price info
+                    da_price = float(soup.find(class_='price large').get_text().strip("$").replace(',', ''))
+                except AttributeError:
+                    da_price = ""
+
+                try:
+                    # Need to get around no eight columns 'NoneType' Object
+                    da_desc = soup.find(class_='eight columns').get_text().strip()  # grab description
+                except AttributeError:
+                    da_desc = soup.find(id='moredetailsTab').get_text().strip()
+
+                da_pic = soup.find(class_='panel radius').findChild()['href']  # grab pic info
+                da_upc = "UPC/Barcode" in soup.find(id='itemdetailsTab').find_all("li")[3].get_text()
+                da_upc_text = ""
+                da_stock_text = ""
+
+                # logic for barcode information
+                if da_upc:
+                    da_upc_text = soup.find(id='itemdetailsTab').find_all("li")[3].get_text().strip("UPC/Barcode:")
+
+                # logic for stock information
+                if da_price == "":
+                    da_stock_text = "Out Of Stock"
+                else:
+                    da_stock_text = "In Stock"
+
+                batch_list.append(
+                    {'range': output_range, 'values': [[title, da_price, da_desc, da_pic, da_upc_text, da_stock_text]]})
+            row += 1
+        da_total_list = sheet.batch_update(batch_list)
+
+if __name__ == "__main__":
+    main()
+
+
+def main_old():
+    print("Starting Scraper")
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:76.0) Gecko/20100101 Firefox/76.0'}
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('tcgcreds.json', scope)
+    client = gspread.authorize(creds)
+    spreadsheet = client.open('Square TCG Scraper Ver1.1')
+    sheet = spreadsheet.get_worksheet(1)
+
 
     rows = sheet.get_all_records()
     #Get all of the URLs in one object
@@ -129,6 +204,7 @@ def main():
         for url in tnt_url_list[start_index:start_index + 10]:  # The first item is a list containing all the information
             tnt_range = "L" + str(row) + ":O" + str(row)
 
+            # TODO: check URL host, create host-specific parse funcs
             if url:
                 print("Reading url info for ", "line:",row, url[0])
 
@@ -197,7 +273,3 @@ def main():
                 cs_batch_list.append({'range': cs_range, 'values': [[cs_title, cs_price, cs_pic, cs_stock_text]]})
             row += 1
         cs_total_list = sheet.batch_update(cs_batch_list)
-
-def
-if __name__ == "__main__":
-    main()
